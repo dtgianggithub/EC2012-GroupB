50ee1fa158abb4110623146b3492a409258cde7f
Đàm Trường Giang <dtgiang1994@gmail.com>
2015-11-29 15:15:15 +0700

    New

---
+++ Sources/TDMT_DOAN/TDMT_DOAN/Controllers/AccountController.cs
@@ -0,0 +1,28 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TDMT_DOAN.Models;
+
+namespace TDMT_DOAN.Controllers
+{
+    public class AccountController : Controller
+    {
+
+        //
+        // GET: /Account/
+        [ChildActionOnly]
+        public ActionResult Index()
+        {
+            return PartialView();
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ Sources/TDMT_DOAN/TDMT_DOAN/Controllers/CartController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TDMT_DOAN.Controllers
+{
+    public class CartController : Controller
+    {
+        //
+        // GET: /Cart/
+        [ChildActionOnly]
+        public ActionResult Index()
+        {
+            return PartialView();
+        }
+
+        public ActionResult CartContent()
+        {
+            return View();
+
+        }
+
+    }
+}
